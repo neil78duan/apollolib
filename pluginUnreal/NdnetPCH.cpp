3eb58c7f6d49fe65d5783d44799c969c6a41b27f
neil78duan <luckduan@126.com>
2019-01-29 15:42:58 +0800

    add pch file

---
+++ pluginUnreal/NdnetPCH.cpp
@@ -0,0 +1,28 @@
+/* file Ndnet.cpp
+ * PCH.h
+ */
+
+
+
+#include "NdnetPCH.h"
+
+#include "Core.h"
+#include "ModuleInterface.h"
+
+
+#include "ModuleInterface.h"
+#include "ModuleManager.h"
+// 
+// IMPLEMENT_MODULE(FNdnet_RuntimeModuleInterface, Ndnet_Runtime);
+// 
+// void FNdnet_RuntimeModuleInterface::StartupModule()
+// {
+// 
+// }
+// 
+// 
+// void FNdnet_RuntimeModuleInterface::ShutdownModule()
+// {
+// 
+// }
+
---
+++ pluginUnreal/NdnetPCH.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+
+/* Private dependencies
+ *****************************************************************************/
+//#define _WINNT_
+
+#include "apollo_client.h"
+
